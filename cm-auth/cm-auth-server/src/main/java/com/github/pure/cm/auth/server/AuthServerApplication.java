ac76640fdc51e66b99f39e96689e25d2a466dca5
ch <1075474353@qq.com>
2020-06-04 10:00:19 +0800

    添加根据maven环境来进行编译

--- cm-auth/cm-auth-server/src/main/java/com/github/pure/cm/auth/server/AuthServerApplication.java
+++ cm-auth/cm-auth-server/src/main/java/com/github/pure/cm/auth/server/AuthServerApplication.java
@@ -2,6 +2,7 @@ package com.github.pure.cm.auth.server;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
--- cm-auth/cm-auth-server/src/main/java/com/github/pure/cm/auth/server/config/auth/RsaConfig.java
+++ cm-auth/cm-auth-server/src/main/java/com/github/pure/cm/auth/server/config/auth/RsaConfig.java
@@ -3,7 +3,12 @@ package com.github.pure.cm.auth.server.config.auth;
 import static com.github.pure.cm.auth.server.config.auth.RsaUtil.*;
 
 import com.github.pure.cm.auth.server.config.auth.RsaUtil.RsaKey;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -12,28 +17,33 @@ import org.springframework.data.redis.core.RedisTemplate;
  * @since 2019/11/18
  */
 @Configuration
+@RefreshScope
+//@ConfigurationProperties(prefix = "")
 public class RsaConfig {
 
-  @Autowired
-  private RedisTemplate<String, String> redisTemplate;
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+    @Value("${custom.config.auth-server.jwt-token-signer.public-key-redis}")
+    private String publicKeyRedis;
+    @Value("${custom.config.auth-server.jwt-token-signer.private-key-redis}")
+    private String privateKeyRedis;
 
-  private final static String REDIS_SERVER_PRIVATE_KEY = "ME:AUTH:SERVER:PRIVATE:KEY";
-  private final static String REDIS_SERVER_PUBLIC_KEY = "ME:AUTH:SERVER:PUBLIC:KEY";
-
-  public RsaKey getRsaKey() {
-    if (redisTemplate.hasKey(REDIS_SERVER_PRIVATE_KEY) && redisTemplate.hasKey(REDIS_SERVER_PUBLIC_KEY)) {
-      String privateKey = redisTemplate.opsForValue().get(REDIS_SERVER_PRIVATE_KEY);
-      String publicKey = redisTemplate.opsForValue().get(REDIS_SERVER_PUBLIC_KEY);
-      RsaKey rsaKey = new RsaKey();
-      rsaKey.setPrivateKey(getPrivateKey(decodeBase64(privateKey)));
-      rsaKey.setPublicKey(getPublicKey(decodeBase64(publicKey)));
-      return rsaKey;
-    } else {
-      RsaKey key = getKey(1024);
-      redisTemplate.opsForValue().set(REDIS_SERVER_PRIVATE_KEY, encodeBase64(key.getPrivateKey()));
-      redisTemplate.opsForValue().set(REDIS_SERVER_PUBLIC_KEY, encodeBase64(key.getPublicKey()));
-      return key;
+    public RsaKey getRsaKey() {
+        System.out.println(this.publicKeyRedis);
+        System.out.println(this.privateKeyRedis);
+        if (redisTemplate.hasKey(publicKeyRedis) && redisTemplate.hasKey(privateKeyRedis)) {
+            String privateKey = redisTemplate.opsForValue().get(publicKeyRedis);
+            String publicKey = redisTemplate.opsForValue().get(privateKeyRedis);
+            RsaKey rsaKey = new RsaKey();
+            rsaKey.setPrivateKey(getPrivateKey(decodeBase64(privateKey)));
+            rsaKey.setPublicKey(getPublicKey(decodeBase64(publicKey)));
+            return rsaKey;
+        } else {
+            RsaKey key = getKey(1024);
+            redisTemplate.opsForValue().set(publicKeyRedis, encodeBase64(key.getPrivateKey()));
+            redisTemplate.opsForValue().set(privateKeyRedis, encodeBase64(key.getPublicKey()));
+            return key;
+        }
     }
-  }
 
 }
