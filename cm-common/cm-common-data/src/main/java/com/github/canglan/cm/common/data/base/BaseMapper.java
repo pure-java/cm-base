de69f8ea8852c2c4f55c08aa6484a2d71d029cf7
bairitan <1075474353@qq.com>
2020-06-20 14:22:29 +0800

    重新上传

--- cm-common/cm-common-data/src/main/java/com/github/canglan/cm/common/data/base/BaseMapper.java
+++
@@ -1,9 +0,0 @@
-package com.github.canglan.cm.common.data.base;
-
-/**
- * @author chenhuan
- * @date 2017-07-12 19:54
- **/
-public interface BaseMapper<T> extends com.baomidou.mybatisplus.core.mapper.BaseMapper<T> {
-
-}
